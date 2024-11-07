a32871e47d161525e46bbeb22858ac1eed6b6b25
Yonathan Nieves <yonathannieves17@gmail.com>
2024-11-07 16:33:28 -0500

    CRUD / Modelo y Controlador de 'Informe' listo

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\salidaController;
+use App\Http\Controllers\API\informeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,10 @@ Route::put('/salidas/{id}', [salidaController::class, 'update']);
 Route::patch('/salidas/{id}', [salidaController::class, 'updatePartial']);
 
 // Rutas de la tabla 'Informes'
+Route::get('/informes', [informeController::class, 'index']);
+Route::post('/informes', [informeController::class, 'store']);
+Route::get('/informes/{id}', [informeController::class, 'show']);
+Route::delete('/informes/{id}', [informeController::class, 'destroy']);
+Route::put('/informes/{id}', [informeController::class, 'update']);
+Route::patch('/informes/{id}', [informeController::class, 'updatePartial']);
+
